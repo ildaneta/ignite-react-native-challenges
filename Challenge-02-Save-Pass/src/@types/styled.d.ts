f089ff9c83d64c82fcd9303d181a6ba315b0c4ca
ildaneta <iildaemanoely@gmail.com>
2021-12-10 10:42:33 -0300

    chore: Adding a new challenge

--- Challenge-02-Save-Pass/src/@types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-import theme from '../global/styles/theme';
-
-
-declare module 'styled-components' {
-  type MyTheme = typeof theme;
-
-  export interface DefaultTheme extends MyTheme { }
-}
